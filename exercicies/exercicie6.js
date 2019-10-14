6b16677132bf3849dbfe93a53abe4be5d552f3fc
Claudia Silva <csilva@allin.com.br>
2019-10-14 11:34:22 -0300

    exercicies 6

--- exercicies/exercicie6.js
+++ exercicies/exercicie6.js
@@ -5,6 +5,12 @@ class Product {
         this.price = price;
         this.discount = discount;
     }
+    resume() {
+        return `${this.name} custa R$${this.price} (${this.discount * 100}% off) valor com desconto ${this.discountPrice()}`;
+    }
+    discountPrice() {
+        return (this.price * (1 - this.discount));
+    }
 }
-const product = new Product('Claudia', 12.00, 1);
-console.log(product);
+const product = new Product('Claudia', 12.00, 0.5);
+console.log(product.resume());
--- exercicies/exercicie6.ts
+++ exercicies/exercicie6.ts
@@ -4,7 +4,14 @@ class Product {
         public price: number, 
         public discount: number = 0
     ) {}
-}
-const product = new Product('Claudia', 12.00, 1)
 
-console.log(product)
+    public resume(): string {                
+        return `${this.name} custa R$${this.price} (${this.discount * 100}% off) valor com desconto ${this.discountPrice()}`
+    }
+
+    public discountPrice(): number {
+        return (this.price * (1 - this.discount))
+    }
+}
+const product = new Product('Claudia', 12.00, 0.5)
+console.log(product.resume())
