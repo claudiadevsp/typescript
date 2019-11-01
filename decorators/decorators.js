ef21a2aaefa7be4afdc2a027dad803ee2db670f9
Claudia Silva <csilva@allin.com.br>
2019-11-01 15:42:50 -0300

    more decorators

--- decorators/decorators.js
+++ decorators/decorators.js
@@ -5,30 +5,38 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function logarClass(constructor) {
+function logClass(constructor) {
     console.log(constructor);
 }
 function decoratorNull(_) { }
-function logarClassSe(value) {
-    return value ? logarClass : decoratorNull;
+function logClassSe(value) {
+    return value ? logClass : decoratorNull;
 }
 function decorator(obj) {
     return function (_) {
         console.log(obj.a + ' ' + obj.b);
     };
 }
-//@logarClass
-let Eletrodomestic = 
-// @decorator({a:'teste', b:123})
-class Eletrodomestic {
+//@logClass
+//@logClassSe(false)
+//@decorator({a:'teste', b:123})
+let Eletrodomestic = class Eletrodomestic {
     constructor() {
     }
 };
 Eletrodomestic = __decorate([
-    logarClassSe(false)
-    // @decorator({a:'teste', b:123})
+    logObject
 ], Eletrodomestic);
 function logObject(constructor) {
+    console.log('Loading ......');
     return class extends constructor {
+        constructor(...args) {
+            console.log('Before ......');
+            super(...args);
+            console.log('After......');
+        }
     };
 }
+new Eletrodomestic();
+new Eletrodomestic();
+new Eletrodomestic();
--- decorators/decorators.ts
+++ decorators/decorators.ts
@@ -1,22 +1,21 @@
 
-function logarClass(constructor: Constructor) {
+function logClass(constructor: Constructor) {
     console.log(constructor)
 }
-
 function decoratorNull(_: Function) {}
 
-function logarClassSe(value: boolean) {
-    return value ? logarClass : decoratorNull
+function logClassSe(value: boolean) {
+    return value ? logClass : decoratorNull
 }
-
 function decorator(obj: {a: string, b?: number}) {
     return function(_: Function): void {
         console.log(obj.a + ' ' + obj.b)
     }
 }
-//@logarClass
-@logarClassSe(false)
-// @decorator({a:'teste', b:123})
+//@logClass
+//@logClassSe(false)
+//@decorator({a:'teste', b:123})
+@logObject
 class Eletrodomestic {
     constructor() {
        
@@ -25,7 +24,15 @@ class Eletrodomestic {
 type Constructor = { new (...args: any[]): {} }
 
 function logObject(constructor: Constructor) {
+    console.log('Loading ......')
     return class extends constructor {
-
+        constructor(...args: any[]) {
+            console.log('Before ......')
+            super(...args)
+            console.log('After......')
+        }
     }
 }
+new Eletrodomestic()
+new Eletrodomestic()
+new Eletrodomestic()
